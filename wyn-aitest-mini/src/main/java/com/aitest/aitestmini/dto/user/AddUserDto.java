c0434397ab96eef94c85be905c5506ead8c40b9e
wangpa1 <3175800689@qq.com>
2021-08-18 18:05:40 +0800

    user

--- wyn-aitest-mini/src/main/java/com/aitest/aitestmini/dto/user/AddUserDto.java
+++ wyn-aitest-mini/src/main/java/com/aitest/aitestmini/dto/user/AddUserDto.java
@@ -1,9 +1,17 @@
 package com.aitest.aitestmini.dto.user;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
-@ApiModel
+@ApiModel(value="注册用户对象",description="用户对象user")
 public class AddUserDto {
+    @ApiModelProperty(value="用户名",required=true)
+    private String userName;
+    @ApiModelProperty(value="密码",required=true)
+    private String password;
+    @ApiModelProperty(value="邮箱",required=true)
+    private String email;
 }
+
