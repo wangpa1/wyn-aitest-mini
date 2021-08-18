408347b15cf39110f22cccfb647abf33be830f31
wangpa1 <3175800689@qq.com>
2021-08-18 11:08:58 +0800

    first commit

---
+++ wyn-aitest-mini/src/main/java/com/aitest/demo/WynApplication.java
@@ -0,0 +1,13 @@
+package com.aitest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WynApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WynApplication.class, args);
+    }
+
+}
---
+++ wyn-aitest-mini/src/test/java/com/aitest/demo/WynApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WynApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
