8067553bd9c0acd1c20f290732a00e164aa98733
wangpa1 <3175800689@qq.com>
2021-08-23 21:16:23 +0800

    sencond

--- wyn-aitest-mini/src/main/java/com/aitest/aitestmini/WynApplication.java
+++ wyn-aitest-mini/src/main/java/com/aitest/aitestmini/WynApplication.java
@@ -2,7 +2,9 @@ package com.aitest.aitestmini;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan("com.aitest.aitestmini.dao")
 @SpringBootApplication
 public class WynApplication {
 
